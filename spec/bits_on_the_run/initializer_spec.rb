c6435f369523ca4c835800601fefa29c1ca5f3cf
Graeme Mathieson <mathie@woss.name>
2009-07-16 15:35:37 +0100

    Avoid repetition.

--- spec/bits_on_the_run/initializer_spec.rb
+++ spec/bits_on_the_run/initializer_spec.rb
@@ -1,36 +1,38 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe BitsOnTheRun::Initializer do
+include BitsOnTheRun
+
+describe Initializer do
   describe "self.run" do
     after(:each) do
       # Reset global state
-      BitsOnTheRun::Configuration.api_key = nil
-      BitsOnTheRun::Configuration.api_secret = nil
+      Configuration.api_key = nil
+      Configuration.api_secret = nil
     end
 
     it "should allow the user to configure an API key and secret" do
-      BitsOnTheRun::Initializer.run do |config|
+      Initializer.run do |config|
         config.api_key = "api key"
         config.api_secret = "api secret"
       end
-      BitsOnTheRun::Configuration.api_key.should == "api key"
-      BitsOnTheRun::Configuration.api_secret.should == "api secret"
+      Configuration.api_key.should == "api key"
+      Configuration.api_secret.should == "api secret"
     end
 
     it "should raise an error if there is no API key specified" do
       lambda {
-        BitsOnTheRun::Initializer.run do |config|
+        Initializer.run do |config|
           config.api_secret = "api secret"
         end
-      }.should raise_error(BitsOnTheRun::ConfigurationError)
+      }.should raise_error(ConfigurationError)
     end
 
     it "should raise an error if there is no API secret specified" do
       lambda {
-        BitsOnTheRun::Initializer.run do |config|
+        Initializer.run do |config|
           config.api_key = "api key"
         end
-      }.should raise_error(BitsOnTheRun::ConfigurationError)
+      }.should raise_error(ConfigurationError)
     end
   end
 end
