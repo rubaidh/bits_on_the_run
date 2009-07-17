b11d8623630a9a8406f5c6694b7955e8ee734174
Mark Connell <mark@rubaidh.com>
2009-07-17 17:24:39 +0100

    add brackets so we don't get a pile of warning messages

--- lib/bits_on_the_run.rb
+++ lib/bits_on_the_run.rb
@@ -7,13 +7,13 @@ require 'rexml/document'
 gem 'activesupport'; require 'active_support'
 gem 'curb'; require 'curb'
 
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'initializer'
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'configuration'
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'client'
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'video'
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'video_create_response'
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'video_template'
-require File.join File.dirname(__FILE__), 'bits_on_the_run', 'video_conversion'
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'initializer')
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'configuration')
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'client')
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'video')
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'video_create_response')
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'video_template')
+require File.join(File.dirname(__FILE__), 'bits_on_the_run', 'video_conversion')
 
 module BitsOnTheRun
 end
